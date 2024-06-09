ecbb569354628c2a1271fe82c67f512cb212aa4c
ksailunaz <ksailunaz@gmail.com>
2024-06-08 21:39:04 -0600

    changed the background color to darkgreen

--- digitalClock.py
+++ digitalClock.py
@@ -4,9 +4,9 @@ from datetime import datetime
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="darkgreen")
 
-label = Label(window, font=("Arial Black",78,"bold"), bg = "steelblue")
+label = Label(window, font=("Arial Black",78,"bold"), bg = "darkgreen")
 label.pack(pady=100)
 
 def clock():
